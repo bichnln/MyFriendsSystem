b9862e684ded628eaac0a61b1b2698c6c388d00a
bichnln <bich.nln@gmail.com>
2019-10-20 17:42:30 +1100

    add unfriend mechanism

---
+++ friendadd.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="description" content="My Friend System"/>
+    <meta name="keywords" content="Assignmetn 2"/>
+    <meta name="author" content="Le Ngoc Bich Nguyen"/>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
