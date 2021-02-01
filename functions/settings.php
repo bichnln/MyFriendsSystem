aef44c8f82fb79b88b9952f4974b7c8689db1bf3
bichnln <bich.nln@gmail.com>
2021-02-01 15:48:38 +1100

    change database connection to cleardb instead of local

--- functions/settings.php
+++ functions/settings.php
@@ -1,9 +1,9 @@
 <?php 
-    // TODO: change to feenix!
-    $host = "localhost";
-    $user = "root";
-    $password = "";
-    $db = "assign2";
+    $cleardb_url = parse_url(getenv("mysql://bde0813befe43f:db931998@us-cdbr-east-03.cleardb.com/heroku_fbb2daf5acdfb47?reconnect=true"));
+    $host = "us-cdbr-east-03.cleardb.com";
+    $user = "bde0813befe43f";
+    $password = "db931998";
+    $db = "heroku_fbb2daf5acdfb47";
 
     $conn = new mysqli($host, $user, $password, $db);
 
